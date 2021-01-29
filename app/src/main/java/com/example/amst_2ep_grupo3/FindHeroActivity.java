6dd684a611fe629944155d22a3ee679ddeae4655
lopez damian <dennyslopez2013@gmail.com>
2021-01-29 12:39:44 -0500

    add screens

--- app/src/main/java/com/example/amst_2ep_grupo3/FindHeroActivity.java
+++ app/src/main/java/com/example/amst_2ep_grupo3/FindHeroActivity.java
@@ -2,13 +2,27 @@ package com.example.amst_2ep_grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FindHeroActivity extends AppCompatActivity {
 
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_hero);
+        context=getApplicationContext();
+    }
+
+    public void buscarHeroe(View view){
+        Intent intent =new Intent(context,HerosActivity.class);
+        String search=((EditText)findViewById(R.id.input_hero_name)).getText().toString();
+        intent.putExtra("search",search);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/amst_2ep_grupo3/HerosActivity.java
+++ app/src/main/java/com/example/amst_2ep_grupo3/HerosActivity.java
@@ -2,13 +2,27 @@ package com.example.amst_2ep_grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HerosActivity extends AppCompatActivity {
+
+    private  String search;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heros);
+
+        context=getApplicationContext();
+
+        Intent intent=getIntent();
+        search=intent.getStringExtra("search");
+
+        Toast.makeText(context,search,Toast.LENGTH_SHORT).show();
+
     }
 }
