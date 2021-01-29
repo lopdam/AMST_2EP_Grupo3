9702c444b67d02eeb3634a8265e240ecd81a719d
dmaroto98 <dmaroto@espol.edu.ec>
2021-01-29 14:31:55 -0500

    buscar heroes

--- app/src/main/java/com/example/amst_2ep_grupo3/HerosActivity.java
+++ app/src/main/java/com/example/amst_2ep_grupo3/HerosActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,11 +17,21 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +43,7 @@ public class HerosActivity extends AppCompatActivity {
     private RequestQueue ListaRequest = null;
     private LinearLayout heroesContainer;
     private Map<String, TextView> heroesMarvel;
+    private HerosActivity contexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,28 +56,36 @@ public class HerosActivity extends AppCompatActivity {
 
         Toast.makeText(context,search,Toast.LENGTH_SHORT).show();
 
+        heroesMarvel = new HashMap<String, TextView>();
+        ListaRequest = Volley.newRequestQueue(this);
+        contexto = this;
 
 
-
-        this.solicitarHerores();
+        this.solicitarHeroes(search);
 
 
     }
 
-    public void solicitarHerores(){
-        String url_registros ="https://www.superheroapi.com/api.php/"+token+"/search/bat";
-        JsonArrayRequest requestRegistros = new JsonArrayRequest(Request.Method.GET, url_registros, null, new Response.Listener<JSONArray>() {
+    public void solicitarHeroes(String name){
+        System.out.println(name);
+        String url_registros ="https://www.superheroapi.com/api.php/"+token+"/search/"+name;
+        JsonObjectRequest requestRegistros = new JsonObjectRequest(Request.Method.GET, url_registros, null, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONArray response) {
-//                mostrarTemperaturas(response);
-//                actualizarGrafico(response);
-                mostrarHeroes(response);
+            public void onResponse(JSONObject  response) {
+                try {
+                    JSONArray myJsonArray = response.getJSONArray("results");
+                    mostrarHeroes(myJsonArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
+//                System.out.println("hola people");
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 System.out.println(error);
+//                System.out.println("erorwaw");
             }
         }) {
             @Override
@@ -80,39 +100,39 @@ public class HerosActivity extends AppCompatActivity {
     }
     private void mostrarHeroes(JSONArray heroes) {
         String registroId;
+        String registroNombre;
         JSONObject registroHeroe;
-        Button nuevoRegistro;
+//        Button nuevoRegistro;
         TextView valorRegistro;
 //        contenedorTemperaturas = findViewById(R.id.cont_temperaturas);
         heroesContainer = findViewById(R.id.heros_container);
         LinearLayout.LayoutParams parametrosLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
         try {
-            for (int i = 0; i < temperaturas.length(); i++) {
-                registroHeroe = (JSONObject) temperaturas.get(i);
+//            JSONArray myJsonArray = heroes.getJSONArray("results");
+            for (int i = 0; i < heroes.length(); i++) {
+                registroHeroe = (JSONObject) heroes.get(i);
                 registroId = registroHeroe.getString("id");
-                if (registroHeroe.getString("key").equals("temperatura")) {
-                    if (heroesMarvel.containsKey(registroId) && fechasTVs.containsKey(registroId)) {
-                        fechaRegistro = fechasTVs.get(registroId);
-                        valorRegistro = heroesMarvel.get(registroId);
-                        fechaRegistro.setText(registroHeroe.getString("date_created"));
-                        valorRegistro.setText(registroHeroe.getString("value") + " C");
-                    } else {
-                        nuevoRegistro = new LinearLayout(this);
-                        nuevoRegistro.setOrientation(LinearLayout.HORIZONTAL);
-                        fechaRegistro = new TextView(this);
-                        fechaRegistro.setLayoutParams(parametrosLayout);
-                        fechaRegistro.setText(registroHeroe.getString("date_created"));
+                registroNombre= registroHeroe.getString("name");
+                System.out.println("hoooooola");
+                System.out.println(registroId);
+                System.out.println(registroNombre);
+//                System.out.println(registroHeroe);
 
-                        valorRegistro = new TextView(this);
-                        valorRegistro.setLayoutParams(parametrosLayout);
-                        valorRegistro.setText(registroHeroe.getString("value") + " C");
-                        nuevoRegistro.addView(valorRegistro);
-                        heroesContainer.addView(nuevoRegistro);
-                        fechasTVs.put(registroId);
-                        heroesMarvel.put(registroId, valorRegistro);
-                    }
-                } else {
-                }
+                        Button myButton = new Button(this);
+                        myButton.setText(registroNombre);
+//
+//                        valorRegistro = new TextView(this);
+//                        valorRegistro.setText(registroHeroe.getString("name"));
+//
+//                        heroesMarvel.put(registroId, valorRegistro);
+
+                        myButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getApplicationContext(), " Listener botón " + v.getTag() , Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        heroesContainer.addView(myButton);
             }
         } catch (JSONException e) {
             e.printStackTrace();
